a70e7d144f5d287c67292e15e19f897664db5031
mztikk <mztikk@outlook.de>
2022-04-09 01:08:00 +0200

    fixed early none return when buffer was empty

--- src/lib.rs
+++ src/lib.rs
@@ -31,18 +31,20 @@ impl Iterator for PathWalker {
         match self.buffer.pop() {
             Some(entry) => Some(entry),
             None => {
-                let entry = self.entries.pop();
+                while self.buffer.is_empty() && !self.entries.is_empty() {
+                    let current_entry = self.entries.pop();
 
-                if let Some(entry) = entry {
-                    if let Ok(paths) = fs::read_dir(entry) {
-                        for path in paths.flatten() {
-                            let entry_path = path.path();
-                            if !entry_path.is_symlink() || self.follow_symlinks {
-                                if entry_path.is_dir() {
-                                    self.entries.push(entry_path);
+                    if let Some(current_entry) = current_entry {
+                        if let Ok(read_dir) = fs::read_dir(&current_entry) {
+                            for entry in read_dir.flatten() {
+                                let entry_path = entry.path();
+                                if !entry_path.is_symlink() || self.follow_symlinks {
+                                    if entry_path.is_dir() {
+                                        self.entries.push(entry_path);
+                                    }
+
+                                    self.buffer.push(entry);
                                 }
-
-                                self.buffer.push(path);
                             }
                         }
                     }
